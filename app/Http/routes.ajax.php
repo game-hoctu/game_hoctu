6b9e2eee5c97390173971646501120219c1b4789
Phong Nguyen <namduong.kh94@gmail.com>
2016-05-25 09:41:14 +0700

    cập nhật route và route ajax

--- app/Http/routes.ajax.php
+++ app/Http/routes.ajax.php
@@ -1,5 +1,31 @@
 <?php
-Route::get('phong/hello', function(){
-	return "Hello";
+//AJAX------------------------------------------------------------------------------------------
+Route::group(['prefix' => 'ajax'], function(){
+	Route::get('albums/list', function(){
+		$data['status'] = 'ERROR';
+		if(App\Albums::all()->count() > 0)
+		{
+			$data['status'] = 'SUCCESS';
+			$data['info'] = App\Albums::all();
+		}
+		return $data;
+	});
+	Route::get('images/{albums_id}/list', function($albums_id){
+		$data['status'] = 'ERROR';
+		$images = new App\Images();
+		$result = $images->where('albums_id', $albums_id)->get();
+		if($result->count() > 0)
+		{
+			$data['status'] = 'SUCCESS';
+			$data['info'] = $result;
+		}
+		return $data;
+	});
+	Route::get('categories/getList', 'CategoriesController@ajaxGetList');
+	Route::get('users/getList', 'UsersController@ajaxGetList');
+	Route::get('images/getList', 'ImagesController@ajaxGetList');
+	Route::get('albums/categories/{cate_id}/list', 'AlbumsController@ajaxAlbumByCate');
+	Route::get('albums/users/{users_id}/list', 'AlbumsController@ajaxAlbumByUsers');
+	Route::get('albums/ajaxGetList', 'AlbumsController@ajaxGetList');
 });
 ?>
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -139,35 +139,3 @@ Route::group(['prefix' => 'admin'], function(){
 	Route::post('users/{id}/ad_postedit',['as'=>'users_postedit','uses'=>'UsersController@ad_postupdate']);
 	Route::get('users/{id}/ad_delete','UsersController@ad_delete');
 });
-
-
-
-//AJAX------------------------------------------------------------------------------------------
-Route::group(['prefix' => 'ajax'], function(){
-	Route::get('albums/list', function(){
-		$data['status'] = 'ERROR';
-		if(App\Albums::all()->count() > 0)
-		{
-			$data['status'] = 'SUCCESS';
-			$data['info'] = App\Albums::all();
-		}
-		return $data;
-	});
-	Route::get('images/{albums_id}/list', function($albums_id){
-		$data['status'] = 'ERROR';
-		$images = new App\Images();
-		$result = $images->where('albums_id', $albums_id)->get();
-		if($result->count() > 0)
-		{
-			$data['status'] = 'SUCCESS';
-			$data['info'] = $result;
-		}
-		return $data;
-	});
-	Route::get('categories/getList', 'CategoriesController@ajaxGetList');
-	Route::get('users/getList', 'UsersController@ajaxGetList');
-	Route::get('images/getList', 'ImagesController@ajaxGetList');
-	Route::get('albums/categories/{cate_id}/list', 'AlbumsController@ajaxAlbumByCate');
-	Route::get('albums/users/{users_id}/list', 'AlbumsController@ajaxAlbumByUsers');
-	Route::get('albums/ajaxGetList', 'AlbumsController@ajaxGetList');
-});
