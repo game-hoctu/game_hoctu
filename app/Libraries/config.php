1290db0a89cbe30dcc63b6ac084d70e48b8efc49
Phong Nguyen <namduong.kh94@gmail.com>
2016-06-07 14:29:18 +0700

    title-bar-class

--- app/Libraries/config.php
+++ app/Libraries/config.php
@@ -1,6 +1,6 @@
 <?php
-//DEFINE('SERVER_PATH', "http://phongnguyen.com/framework/game_hoctu/");
-DEFINE('SERVER_PATH', "http://localhost/thuctap/game_hoctu/");
+DEFINE('SERVER_PATH', "http://phongnguyen.com/framework/game_hoctu/");
+//DEFINE('SERVER_PATH', "http://localhost/thuctap/game_hoctu/");
 DEFINE('UPLOAD_FOLDER', SERVER_PATH."public/upload/images/");
 DEFINE('ALBUM_IMAGE', SERVER_PATH."public/upload/albums/");
 DEFINE('CHILD_IMAGE', SERVER_PATH."public/upload/childs/");
